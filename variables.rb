a6a8e690bfb042667ad40c7e0ecbb07f97a6b8da
Emily Port <emilylport@gmail.com>
2022-08-02 14:23:09 -0600

    Adds sentences from variables

--- variables.rb
+++ variables.rb
@@ -8,3 +8,5 @@ height = 162.56
 current_income = 0.000
 have_dog = true
 have_cat = true
+puts "Hi I'm #{name} and I am #{my_age} years old."
+puts "#{cat_name} is #{cat_age} and contributes #{current_income} in rent."
